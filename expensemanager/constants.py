0512aabd2e121cbf0ced98afa156f8813e0cf03b
DerekYu <derek-nis@hotmail.com>
2017-06-24 17:08:12 -0400

    Connecting pytesseract to the main program successfully

---
+++ expensemanager/constants.py
@@ -0,0 +1,7 @@
+# Grabbing current directory
+import os
+CURRENT_DIRECTORY = os.getcwd()
+
+PYTESSERACT_LOCATION = '/usr/local/Cellar/tesseract/3.05.01/bin/tesseract'
+
+IMAGE_LOCATION = '%s/../test/' % (CURRENT_DIRECTORY)
--- expensemanager/main.py
+++ expensemanager/main.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
+import constants
+
+# required for image processing
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = constants.PYTESSERACT_LOCATION
+
+def main():
+    print text_from_image("text_advanced.gif")
+
+def text_from_image(image_name):
+    return pytesseract.image_to_string(
+        Image.open(constants.IMAGE_LOCATION + image_name)
+    )
+
+def debug():
+    import sys
+    for path in sys.path:
+        print path
+
+main()
