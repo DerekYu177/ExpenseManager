8618439defaeb1057d60f770feef7b8f34997959
DerekYu <derek-nis@hotmail.com>
2017-07-15 13:14:47 -0400

    moving file accessor to a class to simplify interface

--- Modules/file_accessor.py
+++ Modules/file_accessor.py
@@ -11,49 +11,57 @@ import re
 def all_photos_in_location():
     global_variables.RECEIPT_LOCATION = user_interface.prompt_user_for_location()
 
-    photos = find_photos()
 
-    if global_variables.DEBUG:
-        print "All the files located in the receipt location %s are %s" % (
-         global_variables.RECEIPT_LOCATION, print_list_to_string(photos)
-        )
+    photos = PhotoFinder().find_photos()
+
+
 
     return photos
 
-def find_photos():
-    if not global_variables.RECEIPT_LOCATION:
-        raise EnvironmentError('Receipt location not initialized')
+class PhotoFinder:
 
-    photos = []
-    for photo in listdir(global_variables.RECEIPT_LOCATION):
-        if is_photo(photo):
-            photos.append(photo)
+    def __init__(self, ):
+        pass
 
-    return photos
+    def find_photos(self):
+        if not global_variables.RECEIPT_LOCATION:
+            raise EnvironmentError('Receipt location not initialized')
 
-def is_photo(photo_name):
-    if not type(photo_name) is str:
+        photos = []
+        for photo in listdir(global_variables.RECEIPT_LOCATION):
+            if self.is_photo(photo):
+                photos.append(photo)
+
+        if global_variables.DEBUG:
+            print "All the files located in the receipt location %s are %s" % (
+             global_variables.RECEIPT_LOCATION, self.print_list_to_string(photos)
+            )
+
+        return photos
+
+    def is_photo(self, photo_name):
+        if not type(photo_name) is str:
+            return False
+
+        accepted_file_formats = [
+            ".jpg",
+            ".png",
+            ".jpeg",
+        ]
+
+        for file_format in accepted_file_formats:
+            if re.search(re.compile(file_format), photo_name):
+                return True
+
         return False
 
-    accepted_file_formats = [
-        ".jpg",
-        ".png",
-        ".jpeg",
-    ]
+    def print_list_to_string(self, item_list):
+        string = ""
 
-    for file_format in accepted_file_formats:
-        if re.search(re.compile(file_format), photo_name):
-            return True
+        if type(item_list) is list:
+            for item in item_list:
+                string = string + item + "\n"
+        else:
+            string = item_list
 
-    return False
-
-def print_list_to_string(item_list):
-    string = ""
-
-    if type(item_list) is list:
-        for item in item_list:
-            string = string + item + "\n"
-    else:
-        string = item_list
-
-    return string
+        return string
--- Test/file_accessor_test.py
+++ Test/file_accessor_test.py
@@ -4,27 +4,32 @@ from ..Modules import file_accessor as file_accessor
 from ..Modules import shared as shared
 
 class TestMethods(unittest.TestCase):
+    @classmethod
+    def setup_class(cls):
+        global pf
+        pf = file_accessor.PhotoFinder()
+
     def test_print_list_to_string(self):
         test_list = ["a", "b", "c", "d"]
-        actual = file_accessor.print_list_to_string(test_list)
+        actual = pf.print_list_to_string(test_list)
         expected = "a\nb\nc\nd\n"
 
         assert actual == expected
 
     def test_print_list_to_string_when_list_not_list(self):
         test_list = "/Shared/DerekYu177"
-        assert file_accessor.print_list_to_string(test_list) == test_list
+        assert pf.print_list_to_string(test_list) == test_list
 
     def test_find_photos_when_glb_var_not_instantiated_raises_error(self):
         shared.global_variables.RECEIPT_LOCATION = ""
         with pytest.raises(EnvironmentError) as error:
-            file_accessor.find_photos()
+            pf.find_photos()
         error.match(r'Receipt location not initialized')
 
     def test_find_photos(self):
         shared.global_variables.RECEIPT_LOCATION = shared.global_variables.IMAGE_LOCATION
 
-        assert len(file_accessor.find_photos()) == 5
+        assert len(pf.find_photos()) == 5
 
     def test_is_photo(self):
         test_list_of_photos = {
@@ -35,4 +40,4 @@ class TestMethods(unittest.TestCase):
         }
 
         for name, result in test_list_of_photos.iteritems():
-            assert file_accessor.is_photo(name) == result
+            assert pf.is_photo(name) == result
