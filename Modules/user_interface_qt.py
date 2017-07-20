411beccff4a239d7757b1520b7c504749fee2208
DerekYu <derek-nis@hotmail.com>
2017-07-19 22:25:29 -0400

    prototyping the UI with QT, facing difficulties due to macOS & PyQt compatibility issues

--- Modules/user_interface_qt.py
+++ Modules/user_interface_qt.py
@@ -1,33 +1,60 @@
 #!/usr/bin/python2.7
 
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
-# this is a second attempt at building a UI class 
+# this is a second attempt at building a UI class
 # @markusc90 recommended to use PyQt instead of Tk
+
+# to determine what OS it is
+import os
 
 def ui():
     app = QtGui.QApplication(sys.argv)
-    ui = UserInterface()
+    ui = LocationQuery()
     sys.exit(app.exec_())
 
-
-class UserInterface(QtGui.QWidget):
+class LocationQuery(QtGui.QMainWindow):
+    file_location = ""
 
     def __init__(self):
-        super(UserInterface, self).__init__()
+        super(LocationQuery, self).__init__()
 
         self.initUI()
 
     def initUI(self):
-        self.resize(300, 300)
-        self.center()
 
-        self.setWindowTitle('Test')
+        okButton = QtGui.QPushButton("OK")
+        cancelButton = QtGui.QPushButton("Cancel")
+
+        hbox = QtGui.QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(okButton)
+        hbox.addWidget(cancelButton)
+
+        vbox = QtGui.QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('Buttons')
         self.show()
 
-    def center(self):
-        window_frame = self.frameGeometry()
-        center_point = QtGui.QDesktopWidget().availableGeometry().center()
-        window_frame.moveCenter(center_point)
-        self.move(window_frame.topLeft())
+    def file_location_query_button(self):
+        query_button = QtGui.QPushButton('Open', self)
+        query_button.clicked.connect(self.show_dialog)
+        return query_button
+
+    def ok_button(self):
+        ok_button = QtGui.QPushButton('OK', self)
+        ok_button.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        return ok_button
+
+    def show_dialog(self):
+        self.file_location = QtGui.QFileDialog.getOpenFileName(self, 'Open file', '/home')
+
+# for testing purposes
+# if __name__ == '__main__':
+#     ui()
