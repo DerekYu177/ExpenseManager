16b5a0668536480fc71d7c3f1aefe20a44664791
DerekYu <derek-nis@hotmail.com>
2017-07-20 01:38:15 -0400

    adding tests for image_data

---
+++ Test/image_data_test.py
@@ -0,0 +1,31 @@
+import unittest, pytest
+from ..Modules.shared import global_constants
+from ..Modules.shared import global_variables
+from ..Modules.image_data import ImageData
+
+
+class TestMethods(unittest.TestCase):
+    @classmethod
+    def setup_class(cls):
+        global test_data, image_data
+
+        image_data = ImageData({
+            "date": "07/01/17",
+            "time": "20:48:14",
+            "address": "None",
+            "total_amount": "$2017.21",
+            "description": "test data"
+        })
+
+    def test_initializa(self):
+        assert image_data
+
+    def test_as_csv_text(self):
+        expected = "070117-20:48:14,None,$2017.21,test data"
+
+        assert image_data.as_csv_text() == expected
+
+    def test_identifier(self):
+        expected = "070117-20:48:14"
+
+        assert image_data.identifier() == expected
