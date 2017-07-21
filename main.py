a5dd262b4cf09696e93fff660f3cc20e0b557c47
DerekYu <derek-nis@hotmail.com>
2017-07-20 22:55:39 -0400

    writing the execute method

--- main.py
+++ main.py
@@ -7,22 +7,30 @@ import Modules.persistor            as persistor
 import Modules.image_processor      as image_processor
 import Modules.debug                as debug
 import Modules.user_interface       as user_interface
+import Modules.data_file_helper     as data_file_helper
 
 def main():
-    # print image_processor.text_from_image("grocery_store.jpg")
 
     # path = "%s/medium_def_receipt.jpg" % (shared.GlobalVariables.IMAGE_LOCATION)
     #
     # ui = user_interface.UserInterface()
     # ui.display_image(path)
-    print photo_file_finder.all_photos_in_location()
-
-main()
+    execute()
 
 def execute():
     # access list of all photo files
     list_of_photos_files = photo_file_finder.all_photos_in_location()
 
+    data_file_helper.DataFileHelper().initialize_data_file()
+
+    p = persistor.Persistor()
+
     # iterate over all photos in list_of_photos_files
     for photo_file in list_of_photos_files:
-        image_processor.text_from_image(photo_file)
+        image_data = image_processor.image_data_from_image(photo_file)
+
+        if not p.does_data_exist(image_data):
+            p.persist(image_data)
+
+if __name__ == '__main__':
+    main()
