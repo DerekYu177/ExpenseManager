30c6502c813278351916ec9e145132004eb572cd
DerekYu <derek-nis@hotmail.com>
2017-07-22 00:43:53 -0400

    service#begin working as expected. Tests are required for quot-unqot private methods

--- Modules/data_transfer_service.py
+++ Modules/data_transfer_service.py
@@ -1,12 +1,26 @@
-from DataManager import *
-from FileManager import *
-from PhotoAnalyzer import *
-from UI import *
+from DataManager import persistor
+from FileManager import data_file_helper, photo_file_finder
+from PhotoAnalyzer import image_processor
+from UI import user_interface
 
 from shared import *
 from image_data import *
 
 def begin():
-    GlobalVariables.RECEIPT_LOCATION = UI.user_interface.prompt_user_for_location()
+    initialize_files()
 
-    # TODO: here
+    p = persistor.Persistor()
+
+    photos_names = photo_file_finder.get_photo_names()
+
+    for photo_name in photos_names:
+        image_data = image_processor.image_data_from_image(photo_name)
+
+        if not p.does_data_exist(image_data):
+            p.persist_data(image_data)
+
+def initialize_files():
+    GlobalVariables.RECEIPT_LOCATION = user_interface.prompt_user_for_location()
+
+    file_helper = data_file_helper.DataFileHelper()
+    file_helper.initialize_data_file
