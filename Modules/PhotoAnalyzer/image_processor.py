826b4eabab4da7a6762089b748b93422dfad869c
DerekYu <derek-nis@hotmail.com>
2017-08-20 13:12:32 -0400

    Using DateFinder to format dates properly

--- Modules/PhotoAnalyzer/image_processor.py
+++ Modules/PhotoAnalyzer/image_processor.py
@@ -1,7 +1,6 @@
 from PIL import Image as img
 import pytesseract
 import re
-import sys
 
 from ..shared         import GlobalVariables
 from ..shared         import GlobalConstants
@@ -69,8 +68,7 @@ class Finder(object): # new class structure here
         return "find_%s" % (attr)
 
     def find_date(self):
-        date_regex = r'(\d+/\d+/\d+)'
-        return self.find(date_regex)
+        return DateFinder(self.text).find_date()
 
     def find_time(self):
         time_regex = r'(\d+:\d+:\d+)'
@@ -80,16 +78,16 @@ class Finder(object): # new class structure here
         return None # TODO
 
     def find_total_amount(self):
-        return Money(self.text).find_total_amount()
+        return MoneyFinder(self.text).find_total_amount()
 
     def find_description(self):
         return None # TODO
 
-class Money(Finder):
+class MoneyFinder(Finder):
     regex = r'[$]\s*\d+\.\d{2}'
 
     def find_total_amount(self):
-        amounts = super(Money, self).find(self.regex)
+        amounts = super(MoneyFinder, self).find(self.regex)
         return self._max_amounts(amounts)
 
     def _max_amounts(self, money_list):
@@ -135,3 +133,55 @@ class Money(Finder):
             return number + "0"
         else:
             return number
+
+class DateFinder(Finder):
+    regex = r'(\d+/\d+/\d+)'
+    identifier = separator = "/"
+    MAX_MONTH = 12
+    year_range = 1
+
+    def find_date(self):
+        raw_date = super(DateFinder, self).find(self.regex)
+
+        if raw_date is None:
+            return None
+
+        return self._day_month_year(raw_date)
+
+    def _day_month_year(self, date):
+        day, month, year = date.split(self.identifier)
+        day, month, year = self._sanitize_date_data(day, month, year)
+        day, month, year = self._format_date(day, month, year)
+        return day + self.separator + month + self.separator + year
+
+    def _sanitize_date_data(self, day, month, year):
+        year = self._full_year(year)
+        day, month = self._sanitize_day_month(day, month)
+        return day, month, year
+
+    def _full_year(self, year):
+        if len(year) == 2:
+            return int("20" + year)
+        else:
+            return int(year)
+
+    def _sanitize_day_month(self, day, month):
+        day = int(day)
+        month = int(month)
+
+        if (month > self.MAX_MONTH):
+            month, day = day, month
+
+        return day, month
+
+    def _format_date(self, day, month, year):
+        day = self._format_full_length(day)
+        month = self._format_full_length(month)
+        return str(day), str(month), str(year)
+
+    def _format_full_length(self, unit):
+        unit = str(unit)
+        if len(unit) <= 1:
+            return "0" + unit
+        else:
+            return unit
