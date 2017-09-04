dfa38b4029fdda75d8f4202a6b6489167cde2f3a
DerekYu <derek-nis@hotmail.com>
2017-09-04 10:01:05 -0700

    [Fix] TestHelper modifying the correct states

--- test/test_helper.py
+++ test/test_helper.py
@@ -1,16 +1,12 @@
 from ..modules import shared
-from ..modules import debug
 
 # @classmethod
 # def setup_class(cls):
 #     test_helper.prepare()
 
-def prepare():
-    shared.GlobalVariables.DATA_PATH = shared.GlobalConstants.TEST_PERSISTED_DATA_PATH
-    debug.DebugCore.GLOBAL_DEBUG = debug.DebugState.BASIC
-    shared.GlobalVariables.RECEIPT_LOCATION = shared.GlobalVariables.IMAGE_LOCATION
-
 class TestHelper(object):
     @classmethod
     def setup_class(cls):
-        prepare()
+        shared.GlobalVariables.DATA_PATH = shared.GlobalConstants.TEST_PERSISTED_DATA_PATH
+        shared.GlobalVariables.STATE = shared.State.DEBUG_BASIC
+        shared.GlobalVariables.RECEIPT_LOCATION = shared.GlobalVariables.IMAGE_LOCATION
